aced9f2b78ef592989af33e7732a005ffed07e0f
HD0007 <47544051+HD0007@users.noreply.github.com>
2022-08-09 10:06:55 +0530

    Add files via upload

---
+++ src/main/java/com/infosys/webSecurity.java
@@ -0,0 +1,15 @@
+package com.infosys;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@EnableWebSecurity
+public class webSecurity extends WebSecurityConfigurerAdapter {
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+}
+}
